b33c0146b33db1f9f4238ce823aea76a0492af79
JoaoGabriel0511 <joaogabrie0511@gmail.com>
2019-01-05 10:58:11 -0200

    rendering fixes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
-  root 'static_pages#home'
-  get 'static_pages/about'
-  get 'static_pages/services'
-  get 'static_pages/blog'
-  get 'static_pages/contact'
+  get '/home', to: 'static_pages#home'
+  root to: 'static_pages#home'
+  get '/about', to: 'static_pages#about'
+  get '/services', to: 'static_pages#services'
+  get '/blog', to: 'static_pages#blog'
+  get '/contact', to: 'static_pages#contact'
+  get '/test', to: 'static_pages#test'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
