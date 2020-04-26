e05f649b765c740bce9bc4303a8f8e2b9e7fd1eb
JoaoGabriel0511 <joaogabrie0511@gmail.com>
2020-04-26 19:37:29 -0300

    alteracoes

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,5 +1,4 @@
 class Course < ApplicationRecord
     has_many :whens
-    validates_presence_of :description
     validates_presence_of :name
 end
--- app/models/video.rb
+++ app/models/video.rb
@@ -2,5 +2,4 @@ class Video < ApplicationRecord
     validates_presence_of :url
     validates_presence_of :name
     validates :url, format: {with: /^(?:https?:\/\/)?(?:www\.)?youtu(?:\.be|be\.com)\/(?:watch\?v=)?([\w-]{10,})/, :multiline => true}
-    validates_presence_of :description
 end
